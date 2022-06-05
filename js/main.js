bb1f555e8cbde121517c7ccbd82bf5c9d2f683b4
Yuliya Idrisova <julls.idrisova@gmail.com>
2022-06-05 10:41:33 +0300

    Price section and footer added

--- js/main.js
+++ js/main.js
@@ -7,3 +7,10 @@ $(function(){
     autoplaySpeed: 2000,
   });
 });
+
+$(".menu, .bike__column").on("click","a", function (event) {
+  event.preventDefault();
+  const id  = $(this).attr('href'),
+    top = $(id).offset().top;
+  $('body,html').animate({scrollTop: top}, 1500);
+});
